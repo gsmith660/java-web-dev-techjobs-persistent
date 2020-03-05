578c673b1ac361af5be79b8d8447033df04797d1
gsmith660 <gsmith660@gmail.com>
2020-03-04 20:37:32 -0600

    Part 2 Persisting Employers and Skills done

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location cannot be blank.")
+    @Size(max = 255, message = "Location cannot be longer than 255 characters.")
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
